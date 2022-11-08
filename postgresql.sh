0e4c87cc564a7832bf1976809513b5d279840b6d
Ememekong001 <airmyekong@gmail.com>
2022-11-08 21:38:25 +0000

    editted postgres

--- postgresql.sh
+++ postgresql.sh
@@ -1,18 +1,30 @@
-sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" >> /etc/apt/sources.li>
+#!/usr/bin/bash
+#enable oackage repository
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
 
+wget -qO- https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo tee /etc/apt/trusted.gpg.d/pgdg.asc &>/dev/null
 #install postgresql
 
 sudo apt update
 
-sudo apt install postgresql postgresql-contrib
+sudo apt install postgresql postgresql-client -y
 
 #enable postgresql
 
-sudo systemctl postgresql enable
-
-sudo systemctl postgresql status
-
-
+sudo systemctl start postgresql.service
 #setup postgress
 
+P_ACCESS=/etc/postgresql/15/main/pg_hba.conf
+P_CONFIG=/etc/postgresql/15/main/postgresql.conf
 
+#Edit the config file to accept all addresses
+
+echo $'listen_addresses = \'*\' '  >>  $P_CONFIG
+
+# edit the PostgreSQL access policy configuration file
+
+echo "host all all 0.0.0.0/0 md5" | tee -a  $P_ACCESS
+
+systemctl restart postgresql
+
+sudo ufw allow 5432/tcp
